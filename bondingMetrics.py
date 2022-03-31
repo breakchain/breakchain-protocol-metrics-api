cf9e46f2f57cec2e122dfc96a9da9d36710dc27b
theodorebrinkofski <tgb29@cornell.edu>
2022-03-31 17:08:04 -0600

    finish bond metrics

--- bondingMetrics.py
+++ bondingMetrics.py
@@ -5,6 +5,9 @@ import json
 
 def bondMetrics(event, context):
 
+    w3 = Web3(Web3.HTTPProvider('https://rpc-mumbai.matic.today'))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
     # Bond Price
 
     with open("usdcbond_abi.json") as f:
@@ -21,16 +24,44 @@ def bondMetrics(event, context):
     debtRatio = usdcbond.functions.debtRatio().call() / 100
     print("Debt Ratio: " + str(debtRatio))
 
+    with open("xchain_abi.json") as f:
+        xchain_abi = json.load(f)
+
+    # Temp ROI. Change to market price in the future
+    xchain = w3.eth.contract(
+        address='0xd3d2d5F7582b53F0929D06fB6f12473811ba2aB5', abi=xchain_abi)
+
+    totalSupplyXchain = (xchain.functions.totalSupply().call())*(10**9)
+    with open("usdc_abi.json") as f:
+        usdc_abi = json.load(f)
+    usdc = w3.eth.contract(
+        address='0x9a7E7639322643d02F8CB478baB0EB1019F82389', abi=usdc_abi)
+    treasuryAddress = "0x08BC282d0970b19c7E86E6fa35693e321F9E5B50"
+    treasuryUSDCbalance = usdc.functions.balanceOf(treasuryAddress).call()
+    priceFloor = (treasuryUSDCbalance / totalSupplyXchain)
+
+    roi = (priceFloor - bondPrice) / priceFloor
+
+    # max price
+
+    maxPrice = usdcbond.functions.maxPayout().call() / 10**9
+
+    # You will get == (amount deposited / bond price)
+
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "metric1": 1,
-                "metric2": 2
+                "you-will-get": "",
+                "max-you-can-buy": maxPrice,
+                "ROI": roi,
+                "debt-ratio": debtRatio,
+                "vesting-term": "5 Days"
+
             }
 
         )
     }
 
 
-print(bondMetrics())
+print(bondMetrics("event", "context"))
